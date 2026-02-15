b1b99bbd2617953b71ee216cb6bfe0e46372c64a
TanHongZhe <hongzhetan7@gmail.com>
2026-02-15 13:40:00 +0000

    Update

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,5 +4,9 @@ export default {
             domain: "https://upright-filly-16.clerk.accounts.dev",
             applicationID: "convex",
         },
+        {
+            domain: "https://clerk.wavelength.lol",
+            applicationID: "convex",
+        },
     ],
 };
